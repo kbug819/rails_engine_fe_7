74730605259f31e2cb7f4410f716a9eb85c985ef
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-26 00:14:59 -0500

    update to merchants facade

--- app/facades/merchants_facade.rb
+++ app/facades/merchants_facade.rb
@@ -1,13 +1,21 @@
 class MerchantsFacade
-  attr_reader :merchant_id
+  attr_reader :merchant_id, :search_by_merchant, :search_term
 
   def initialize(merchant_info)
     @merchant_id = merchant_info[:id]
+    @search_by_merchant = merchant_info[:commit]
+    @search_term = merchant_info[:name]
   end
 
   def merchant_index
-    RailsEngineService.new.merchant_list.map do |merchant|
-      Merchant.new(merchant)
+    if @search_by_merchant
+      RailsEngineService.new.merchant_search(@search_term).map do |merchant|
+        Merchant.new(merchant)
+      end
+    else
+      RailsEngineService.new.merchant_list.map do |merchant|
+        Merchant.new(merchant)
+      end
     end
   end
 
