76c8d1b8d3a0d5018dfa2997c14a3ea66423735c
Kaylee Janes <kaylee.j.janes@gmail.com>
2023-10-25 23:47:56 -0500

    route setup

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/merchants', to: 'merchants#index'
+  get '/merchants/:id', to: 'merchants#show', as: 'merchant'
+  get '/items/:id', to: 'items#show', as: 'item'
 end
